e587037933b66d8754ae44200b0446978255bdc6
artbecker <arthurk.becker@gmail.com>
2025-05-13 19:47:07 -0300

    ajustes para o deploy

--- front-end/api/api.js
+++ front-end/api/api.js
@@ -2,7 +2,7 @@ import "dotenv/config";
 import axios from "axios";
 
 const { NODE_ENV } = process.env;
-const URL = NODE_ENV === "DEVELOPMENT" ? "http://localhost:3000/api" : "/api";
+const URL = NODE_ENV === "development" ? "http://localhost:3000/api" : "/api";
 
 const responseArtists = await axios.get(`${URL}/artists`);
 const responseSongs = await axios.get(`${URL}/songs`);
